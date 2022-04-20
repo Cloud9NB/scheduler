3ceb806c96347a1a37114fafd4cfc7dc9a82809b
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-04-19 21:34:22 -0700

    delete coments

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -18,32 +18,3 @@ export default function DayList(props) {
       </ul>
     );
 };
-
-// export default function DayList(props){
-
-//   return(
-//     <ul>
-//       <DayListItem 
-//         key={props.days[0].id}
-//         name={props.days[0].name} 
-//         spots={props.days[0].spots} 
-//         selected={props.days[0].name === props.day}
-//         setDay={props.setDay}  
-//       />
-//       <DayListItem
-//         key={props.days[1].id} 
-//         name={props.days[1].name} 
-//         spots={props.days[1].spots} 
-//         selected={props.days[1].name === props.day}
-//         setDay={props.setDay}  
-//       />
-//       <DayListItem 
-//         key={props.days[2].id}
-//         name={props.days[2].name}
-//         spots={props.days[2].spots} 
-//         selected={props.days[2].name === props.day}
-//         setDay={props.setDay}  
-//       />      
-//     </ul>
-//   )
-// };
