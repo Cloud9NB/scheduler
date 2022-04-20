a2136aa8eca14ebbfc47ede29cdb8c38481e4a02
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-04-19 17:03:07 -0700

    add DayListItem component and import it to index.js

--- stories/index.js
+++ stories/index.js
@@ -6,6 +6,7 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+import DayListItem from "components/Button";
 
 storiesOf("Button", module)
   .addParameters({
