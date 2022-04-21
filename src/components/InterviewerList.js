76e40e0d29e1b718495a9b2ffad9f81faca79d2d
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-04-20 17:41:56 -0700

    add interviewlist / interviewlistitem component and scripts for them, and add stories for them

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList (props) {
+  console.log('InterviwerList: ', props);
+
+  const listOfInterviewers = props.interviewers.map((x) =>
+    <InterviewerListItem
+      key={x.id}
+      name={x.name}
+      avatar={x.avatar}
+      selected={props.interviewer === x.id}
+      setInterviewer={props.setInterviewer}
+    />
+  );
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {listOfInterviewers}
+      </ul>
+    </section>
+  )
+};
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "components/InterviewerListItem.scss"
+import classNames from "classnames";
+
+export default function InterviewerListItem (props) {
+  // console.log('interviewerlistitem: ', props);
+  const clickInterviewer = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  })
+
+  return (
+    <li onClick={() => props.setInterviewer(props.id)} className={clickInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  )
+};
