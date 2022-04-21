3deba25a5dbf7c83d7bc5c37e6ad76b26896ec69
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-04-21 00:48:01 -0700

    add Header and make a ternary operator to show Show or Empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,9 @@ import Empty from "./Empty";
 
 export default function Appointment (props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      { props.interview ? <Show/> : <Empty/> }
+    </article>
   );
 };
