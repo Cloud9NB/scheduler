2d2fcf594a162f33cba6b4994768d6c94182af1a
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-04-21 22:59:33 -0700

    manage the state

--- src/components/Application.js
+++ src/components/Application.js
@@ -51,8 +51,14 @@ const appointment = Object.values(appointments).map((appt) =>
 );
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days) => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     const apiDays = `http://localhost:8001/api/days`
@@ -74,8 +80,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         />
       </nav>
