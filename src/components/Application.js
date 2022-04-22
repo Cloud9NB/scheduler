3198e565d5ee07d9e6e1dcda6182b37cb112934b
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-04-21 18:27:23 -0700

    install/import axios / useEffect and make days useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "components/Appointment";
+import axios from "axios";
 
-const days = [
-  {
+const appointments = {
+  "1": {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
-  {
+  "2": {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
-  {
+  "3": {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
-];
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+const appointment = Object.values(appointments).map((appt) =>
+  <Appointment
+    key={appt.id}
+    {...appt}
+  />
+);
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+  
 
   return (
     <main className="layout">
@@ -46,7 +79,8 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointment}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
