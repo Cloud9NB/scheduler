afb3a6fa23da29e7f1cc9bf9487ef9038ea00f3a
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-04-23 12:34:14 -0700

    implement transition function for the onAdd click

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,13 +8,14 @@ import useVisualMode from "hooks/useVisualMode";
 export default function Appointment (props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE";
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
 
   return (
     <article className="appointment">
       <Header time={props.time}/>
       {/* { props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/> } */}
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
       <Show
         student={props.interview.student}
